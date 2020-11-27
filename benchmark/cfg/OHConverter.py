2d25e97375a62ebb405f0945f88a09a47527eed1
rmazzine <mazzine.r@gmail.com>
2020-11-27 14:12:05 +0100

    Allow reduced to OH conversion
    This commit adds the feature to convert reduced form data to OH.

--- benchmark/cfg/OHConverter.py
+++ benchmark/cfg/OHConverter.py
@@ -7,6 +7,12 @@ class Converter:
         self.oh_col = oh_col
         self.oh_dict = {}
         self.cat_feats = cat_feats
+        self.binary_cats = []
+
+        for cat_f in cat_feats:
+            if len(full_df[cat_f].unique()) == 2:
+                self.binary_cats.append(cat_f)
+
         for cat_f in cat_feats:
             values = list(full_df[cat_f].unique())
             for v in values:
@@ -23,6 +29,38 @@ class Converter:
                 else:
                     print('Warning - the key was not found, it will considered a binary')
                     self.oh_dict[cat_f] = {cat_f: 'binary'}
+
+        self.dict_feat_idx = {}
+        for idx, col in enumerate(self.oh_col):
+            cat_name = col.split('_')[0]
+            value_name = '_'.join(col.split('_')[1:])
+            if cat_name != 'output':
+                if str(int(cat_name)) in self.dict_feat_idx.keys():
+                    self.dict_feat_idx[str(int(cat_name))][value_name] = idx
+                else:
+                    self.dict_feat_idx[str(int(cat_name))] = {value_name: idx}
+
+    def convert_to_oh(self, data):
+        out_data = [0]*(len(self.oh_col) - 1)
+
+        for idx, c_value in enumerate(data):
+            if str(idx) in self.cat_feats and str(idx) not in self.binary_cats:
+                idx_oh = self.dict_feat_idx[str(idx)][c_value]
+
+                out_data[idx_oh] = 1
+            else:
+                try:
+                    idx_oh = self.oh_col.index(str(idx))
+                except ValueError:
+                    len_idx = len(str(idx))
+                    extended_idx = '0'*(4 - len_idx)+str(idx)
+                    idx_oh = self.oh_col.index(extended_idx)
+
+                out_data[idx_oh] = c_value
+
+        return out_data
+
+
 
     def convert(self, data):
         if type(data) == pd.DataFrame:
