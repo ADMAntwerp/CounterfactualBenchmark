[BUGFIX] - Add TF session, handle dif. TF v., fix numerical only
This commit has several fixes. First it now can handle different TensorFlow versions (1 and 2) as some settings are specific for tensorflow 2 or 1. It also adds a session variable because some CF generation frameworks need the TF session. It also adds the DF for the factual points. Finally it fixes a bug when handling numerical only data.

benchmark/benchmark_template.py
@@ -1,5 +1,6 @@
 import random as python_random
 import operator
+import os
 
 import sys
 
@@ -20,7 +21,10 @@ from constants.var_types import VAR_TYPES
 # Set seeds
 np.random.seed(42)
 python_random.seed(42)
-tf.random.set_seed(42)
+try:
+    tf.random.set_random_seed(42)
+except AttributeError:
+    tf.random.set_seed(42)
 tf.compat.v1.disable_v2_behavior()  # disable TF2 behaviour as alibi code still relies on TF1 constructs
 
 print('TF version: ', tf.__version__)
@@ -28,10 +32,13 @@ print('Eager execution enabled: ', tf.executing_eagerly())  # False
 
 # Disable GPU
 # Disable all GPUS
-tf.config.set_visible_devices([], 'GPU')
-visible_devices = tf.config.get_visible_devices()
-for device in visible_devices:
-    assert device.device_type != 'GPU'
+try:
+    tf.config.set_visible_devices([], 'GPU')
+    visible_devices = tf.config.get_visible_devices()
+    for device in visible_devices:
+        assert device.device_type != 'GPU'
+except AttributeError:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
 def _nn_ohe(input_shape, hidden_layers_ws, output_number):
@@ -113,106 +120,107 @@ def run_experiment(framework_name, framework_tester, output_number=1, ds_id_test
             cfs_framework = []
             for idx_cf in range(df_factual.shape[0]):
 
-                # Load model
-                model_keras = load_model(f'../models/{dsName}.h5', compile=False)
+                with tf.compat.v1.Session() as session:
 
-                # Get architecture info and pass to our NN architecture
-                input_shape = model_keras.get_weights()[0].shape[0]
-                hidden_layers_ws = model_keras.get_weights()[0].shape[1]
+                    # Load model
+                    model_keras = load_model(f'../models/{dsName}.h5', compile=False)
 
-                adapted_nn = _nn_ohe(input_shape, hidden_layers_ws, output_number)
-                adapted_nn.build(input_shape=input_shape)
+                    # Get architecture info and pass to our NN architecture
+                    input_shape = model_keras.get_weights()[0].shape[0]
+                    hidden_layers_ws = model_keras.get_weights()[0].shape[1]
 
-                adapted_nn.layers[1].set_weights(model_keras.layers[0].get_weights())
-                adapted_nn.layers[2].set_weights(model_keras.layers[1].get_weights())
+                    adapted_nn = _nn_ohe(input_shape, hidden_layers_ws, output_number)
+                    adapted_nn.build(input_shape=input_shape)
 
-                if c == 1:
-                    # If the tested class is the minority, revert output order
-                    second_layer_w = []
-                    for w_row in model_keras.layers[1].get_weights()[0]:
-                        second_layer_w.append(np.flip(w_row).tolist())
+                    adapted_nn.layers[1].set_weights(model_keras.layers[0].get_weights())
+                    adapted_nn.layers[2].set_weights(model_keras.layers[1].get_weights())
 
-                    second_layer_b = np.flip(model_keras.layers[1].get_weights()[1])
+                    if c == 1:
+                        # If the tested class is the minority, revert output order
+                        second_layer_w = []
+                        for w_row in model_keras.layers[1].get_weights()[0]:
+                            second_layer_w.append(np.flip(w_row).tolist())
 
-                    second_layer = [np.array(second_layer_w, dtype=np.float32),
-                                    np.array(second_layer_b, dtype=np.float32)]
-                    adapted_nn.layers[2].set_weights(second_layer)
+                        second_layer_b = np.flip(model_keras.layers[1].get_weights()[1])
 
-                # The original model has 2 outputs, therefore, the weights must be modified to output only one
-                if output_number == 1:
-                    adapted_nn.layers[3].set_weights(
-                        [np.array([[0.0], [1.0]], dtype=np.float32), np.array([0.0], dtype=np.float32)])
+                        second_layer = [np.array(second_layer_w, dtype=np.float32),
+                                        np.array(second_layer_b, dtype=np.float32)]
+                        adapted_nn.layers[2].set_weights(second_layer)
 
-                # If there are categorical features, create a CF OH entry
-                if cat_feats:
-                    converter = OHConverter.Converter(df, cat_feats, list(df_oh.columns))
-                else:
-                    converter = lambda x: x
+                    # The original model has 2 outputs, therefore, the weights must be modified to output only one
+                    if output_number == 1:
+                        adapted_nn.layers[3].set_weights(
+                            [np.array([[0.0], [1.0]], dtype=np.float32), np.array([0.0], dtype=np.float32)])
 
-                factual = df_factual.drop(columns=['output']).iloc[idx_cf].to_list()
-                factual_oh = converter.convert_to_oh(df_factual.drop(columns=['output']).iloc[idx_cf].to_list()) if cat_feats else factual
-
-                print(f'CF GENERATION: DATA - {dsName} / C - {c} /ROW - {idx_cf}')
-                cf_out, cf_time = framework_tester(df_train, df_oh_train, df_test, df_oh_test, num_feats, cat_feats,
-                                                   converter, adapted_nn, factual, factual_oh)
-
-                # Verify if counterfactual (cf) is a list, the output MUST a list
-                try:
-                    assert type(cf_out) == list
-                except AssertionError:
-                    raise AssertionError('The counterfactual output must be a list')
-                # Verify if counterfactual (cf) is one-hot encoded or not, output must be always one-hot encoded
-                try:
-                    assert len(cf_out) in [len(factual), len(factual_oh), 0]
-                except AssertionError:
-                    raise AssertionError(f'The counterfactual output must be the same size as the factual '
-                                         f'({len(factual)}) or one-hot encoded factual ({len(factual_oh)}) or zero')
-
-                # Measure time to get CF only if we get a cf
-                if len(cf_out) > 0:
-                    timeRunFramework = [idx_cf, int(c), dsName, cf_time]
-                    pd.DataFrame(timeRunFramework).T.to_csv(f'../cfoutput/TIME_{framework_name}.csv', mode='a',
-                                                            header=False, index=False)
-
-                # Append result to result array and verify if it's a counterfactual
-                if len(cf_out) > 0:
-
-                    # Saved result must always be one-hot encoded
-                    if len(cf_out) != len(factual_oh):
-                        cf_out_oh = converter.convert_to_oh(factual)
-                    else:
-                        cf_out_oh = cf_out
-
+                    # If there are categorical features, create a CF OH entry
                     if cat_feats:
-                        factual_class = adapted_nn.predict(np.array([factual_oh]))[0]
-                        cf_class = adapted_nn.predict(np.array([cf_out_oh]))[0]
+                        converter = OHConverter.Converter(df, cat_feats, list(df_oh.columns))
                     else:
-                        factual_class = adapted_nn.predict(np.array([factual_oh]))[0]
-                        cf_class = adapted_nn.predict(np.array([cf_out_oh]))[0]
+                        converter = lambda x: x
 
-                    if output_number == 2:
+                    factual = df_factual.drop(columns=['output']).iloc[idx_cf].to_list()
+                    factual_oh = converter.convert_to_oh(df_factual.drop(columns=['output']).iloc[idx_cf].to_list()) if cat_feats else factual
+
+                    print(f'CF GENERATION: DATA - {dsName} / C - {c} /ROW - {idx_cf}')
+                    cf_out, cf_time = framework_tester(df_train, df_oh_train, df_test, df_oh_test, num_feats, cat_feats,
+                                                       converter, adapted_nn, df_factual, factual, factual_oh, session)
+
+                    # Verify if counterfactual (cf) is a list, the output MUST a list
+                    try:
+                        assert type(cf_out) == list
+                    except AssertionError:
+                        raise AssertionError('The counterfactual output must be a list')
+                    # Verify if counterfactual (cf) is one-hot encoded or not, output must be always one-hot encoded
+                    try:
+                        assert len(cf_out) in [len(factual), len(factual_oh), 0]
+                    except AssertionError:
+                        raise AssertionError(f'The counterfactual output must be the same size as the factual '
+                                             f'({len(factual)}) or one-hot encoded factual ({len(factual_oh)}) or zero')
+
+                    # Measure time to get CF only if we get a cf
+                    if len(cf_out) > 0:
+                        timeRunFramework = [idx_cf, int(c), dsName, cf_time]
+                        pd.DataFrame(timeRunFramework).T.to_csv(f'../cfoutput/TIME_{framework_name}.csv', mode='a',
+                                                                header=False, index=False)
+
+                    # Append result to result array and verify if it's a counterfactual
+                    if len(cf_out) > 0:
+
+                        # Saved result must always be one-hot encoded
+                        if len(cf_out) != len(factual_oh):
+                            cf_out_oh = converter.convert_to_oh(factual)
+                        else:
+                            cf_out_oh = cf_out
+
                         if cat_feats:
+                            factual_class = adapted_nn.predict(np.array([factual_oh]))[0]
+                            cf_class = adapted_nn.predict(np.array([cf_out_oh]))[0]
+                        else:
+                            factual_class = adapted_nn.predict(np.array([factual_oh]))[0]
+                            cf_class = adapted_nn.predict(np.array([cf_out_oh]))[0]
+
+                        if output_number == 2:
                             factual_class = factual_class[1]
                             cf_class = cf_class[1]
                         else:
                             factual_class = factual_class[0]
                             cf_class = cf_class[0]
 
-                    # Check if the factual class is indeed 0, if not, it's a internal problem that must be reported
-                    try:
-                        assert factual_class < 0.5
-                    except AssertionError:
-                        raise AssertionError('Internal error, original class is wrong. Please, report this error.')
+                        # Check if the factual class is indeed 0, if not, it's a internal problem that must be reported
+                        try:
+                            assert factual_class < 0.5
+                        except AssertionError:
+                            raise AssertionError('Internal error, original class is wrong. Please, report this error.')
 
-                    if cf_class >= 0.5:
-                        print(f'Counterfactual found!\nFactual class:{factual_class}\nCF class:{cf_class}\n')
+                        if cf_class >= 0.5:
+                            print(f'Counterfactual found!\nFactual class:{factual_class}\nCF class:{cf_class}\n')
+                        else:
+                            print(f'Failed counterfactual!\nFactual class:{factual_class}\nCF class:{cf_class}\n')
+
+                        cfs_framework.append(cf_out_oh)
                     else:
-                        print(f'Failed counterfactual!\nFactual class:{factual_class}\nCF class:{cf_class}\n')
-
-                    cfs_framework.append(cf_out_oh)
-                else:
-                    print(f'No returned counterfactual candidate!\n')
-                    cfs_framework.append([np.NaN] * (len(factual_oh)))
+                        print(f'No returned counterfactual candidate!\n')
+                        cfs_framework.append([np.NaN] * (len(factual_oh)))
 
                 tf.keras.backend.clear_session()
 