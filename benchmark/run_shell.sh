11d238d4f5b68d0b366b900d7a8301f04b526619
rmazzine <mazzine.r@gmail.com>
2021-01-29 13:02:56 +0100

    Create log for benchmark runs in parallel shell runs
    This commit implements logging for benchmark runs. It is important as several parallel runs may make difficult to identify possible problems.

--- benchmark/run_shell.sh
+++ benchmark/run_shell.sh
@@ -1,3 +1,6 @@
-if ! python $1 $2; then
+if ! python $1 $2 ; then
   echo 'ERROR in '$2
+  echo 'ERROR in ' $1 ' AND DSNAME ' $2 >> ./log_bench/$1.log
+else
+  echo 'SUCCESS in ' $1 ' AND DSNAME ' $2 >> ./log_bench/$1.log
 fi
