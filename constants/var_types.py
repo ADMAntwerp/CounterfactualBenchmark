e385ba3e1f5992c95b677ecdfe7599324d834ca3
rmazzine <mazzine.r@gmail.com>
2021-02-15 17:03:25 +0100

    Remove Lenses Dataset
    Lenses Dataset didn't have enough rows to make statistical significant tests, therefore, it was removed.

--- constants/var_types.py
+++ constants/var_types.py
@@ -15,10 +15,6 @@ VAR_TYPES = {
         'categorical': ['0', '1', '2', '3', '4', '5'],
         'numerical': []
     },
-    'Lenses': {
-        'categorical': ['0', '1', '2', '3'],
-        'numerical': []
-    },
     'Lymphography': {
         'categorical': ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'],
         'numerical': []
