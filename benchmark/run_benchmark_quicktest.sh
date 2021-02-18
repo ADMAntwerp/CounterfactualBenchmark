2ac5cab8cdc5452952639aba27e1ea484db1a1a7
rmazzine <mazzine.r@gmail.com>
2021-02-18 18:43:53 +0100

    [BUGFIX] Add sleep time before activating conda environments
    This bug is caused because the previous code was activating just after the environment was created, then generating an error. This commit adds a sleep time of 10 seconds that solves that.

--- benchmark/run_benchmark_quicktest.sh
+++ benchmark/run_benchmark_quicktest.sh
@@ -15,39 +15,57 @@ fi
 # Create All Conda Environments Needed
 echo "Creating conda environments"
 conda create --name ALIBIC python=3.7 -y &&
+conda init &&
+sleep 10
 conda activate ALIBIC &&
 pip install -r ../framework_requirements/alibic_requirements.txt &&
 
 conda create --name CADEX python=3.6 -y &&
+conda init &&
+sleep 10
 conda activate CADEX &&
 pip install -r ../framework_requirements/cadex_requirements.txt &&
 
 conda create --name DICE python=3.7 -y &&
+conda init &&
+sleep 10
 conda activate DICE &&
 pip install -r ../framework_requirements/dice_requirements.txt &&
 
 conda create --name GROWINGSPHERES python=3.6 -y &&
+conda init &&
+sleep 10
 conda activate GROWINGSPHERES &&
 pip install -r ../framework_requirements/growingspheres_requirements.txt &&
 
 conda create --name LORE python=3.7 -y &&
+conda init &&
+sleep 10
 conda activate LORE &&
 pip install -r ../framework_requirements/lore_requirements.txt &&
 
 conda create --name MACE python=3.6 -y &&
+conda init &&
+sleep 10
 conda activate MACE &&
 pysmt-install --z3 --confirm-agreement &&
 pip install -r ../framework_requirements/mace_requirements.txt &&
 
 conda create --name MLEXPLAIN python=3.7 -y &&
+conda init &&
+sleep 10
 conda activate MLEXPLAIN &&
 pip install -r ../framework_requirements/mlexplain_requirements.txt &&
 
 conda create --name SEDC python=3.7 -y &&
+conda init &&
+sleep 10
 conda activate SEDC &&
 pip install -r ../framework_requirements/sedc_requirements.txt &&
 
 conda create --name SYNAS python=3.7 -y &&
+conda init &&
+sleep 10
 conda activate SYNAS &&
 pip install -r ../framework_requirements/synas_requirements.txt &&
 
