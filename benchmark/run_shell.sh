3d012aefead8d72d54254745edb6fc2fbd3fe578
rmazzine <mazzine.r@gmail.com>
2021-03-17 14:00:01 +0100

    Remove unused scripts

--- benchmark/run_shell.sh
+++
@@ -1,9 +0,0 @@
-init_date=$(date +%s)
-if ! timeout 300 python $1 $2 $3 $4 ; then
-  final_date=$(date +%s)
-  total_time=$((final_date-init_date))
-  echo 'PROCESS TOOK' $total_time 'SECONDS' 'ERROR in '$2 $3 $4
-  echo 'PROCESS TOOK' $total_time 'SECONDS' 'ERROR in ' $1 ' AND DSNAME ' $2 'FOR C' $3 'AND ROW' $4 >> ./log_bench/$1_$2.log
-else
-  echo 'SUCCESS in ' $1 ' AND DSNAME ' $2 >> ./log_bench/$1_$2.log
-fi
--- benchmark/run_shell_multiple.sh
+++
@@ -1,4 +0,0 @@
-for DSIDX in $(seq $1 $2)
-do
-  sh run_shell.sh $3 $4 $5 $DSIDX &
-done
