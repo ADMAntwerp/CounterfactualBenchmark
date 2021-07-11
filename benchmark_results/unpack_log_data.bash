96f002191201a4ee051db4f5d2ce3b3a60ff4581
rmazzine <mazzine.r@gmail.com>
2021-07-11 10:38:04 +0200

    Add code to unpack experiment data

---
+++ benchmark_results/unpack_log_data.bash
@@ -0,0 +1,3 @@
+tar -C ../benchmark/ -xzvf ../benchmark/full_benchmark_log/log_0_50.tar.gz
+tar -C ../benchmark/ -xzvf ../benchmark/full_benchmark_log/log_50_100.tar.gz
+tar -C ../benchmark/ -xzvf ../benchmark/full_benchmark_log/log_ds_20.tar.gz
