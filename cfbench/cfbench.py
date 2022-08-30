7446aba1e3b27ff66bad30d51685fc4d9e180831
rmazzine <mazzine.r@gmail.com>
2022-08-30 14:16:39 +0200

    Implement result processing and upload

--- cfbench/cfbench.py
+++ cfbench/cfbench.py
@@ -1,64 +1,26 @@
-from cfbench.benchmark_template_row_instance import BenchmarkGenerator
+import os
+import shutil
 
+import pandas as pd
+
+from cfbench.benchmark_template_row_instance import BenchmarkGenerator
+from cfbench.dataset_data.constants.var_types import VAR_TYPES
+
+CURRENT_PATH = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+
+total_factual = 0
+for factual_class in ['0', '1']:
+    for dsname in VAR_TYPES.keys():
+        test_df = pd.read_csv(
+            f'{CURRENT_PATH}/dataset_data/experiments_data/{dsname}_CFDATASET_{factual_class}.csv')
+        total_factual += pd.read_csv(
+            f'{CURRENT_PATH}/dataset_data/experiments_data/{dsname}_CFDATASET_{factual_class}.csv').shape[0]
 
 class BenchmarkCF:
 
-    def __init__(self, framework_name: str, output_number: int = 1, disable_gpu: bool = False):
-        self.framework_name = framework_name
+    def __init__(self, output_number: int = 1, disable_gpu: bool = False):
         self.output_number = output_number
         self.disable_gpu = disable_gpu
-
-    def framework_wrapper(
-            self,
-            df_train,
-            df_oh_train,
-            df_test,
-            df_oh_test,
-            num_feats,
-            cat_feats,
-            converter,
-            adapted_nn,
-            df_factual,
-            factual,
-            factual_oh,
-            session):
-        """
-        Function used to benchmark counterfactual explanation generation algorithms. It includes most data one
-        generator may use, although it's not needed to use all them. Please, report if you think any additional
-        data should be provided. This function output must be a simple list with the counterfactual result and the
-        time used to generate
-        it.
-
-        :param df_train: The train dataframe, including the output class
-        :param df_oh_train: Same as df_train but one-hot encoded, IF THERE'S NO CATEGORICAL
-        FEATURES IT'S THE SAME AS df_train
-        :param df_test: The test dataframe, including the output class
-        :param df_oh_test: Same as df_test but one-hot encoded, IF THERE'S NO CATEGORICAL FEATURES
-        IT'S THE SAME AS df_test
-        :param num_feats: A list with the column names of numerical features
-        :param cat_feats: A list with the column names of categorical features
-        :param converter: Converter from/to one-hot encoded.
-                            Conversion non-encoded -> one-hot encoded: converter.convert_to_oh(INPUT_DATA_NON_ENCODED).
-                            Conversion one-hot encoded -> non-encoded: converter.convert(INPUT_DATA_OH_DATA).
-                            * INPUT_DATA_NON_ENCODED and INPUT_DATA_OH_DATA must be simple lists
-                            * INPUT_DATA_NON_ENCODED and INPUT_DATA_OH_DATA must follow the same column structure as
-                            df_train or df_oh_train
-                            -> PROPERTY: converter.binary_cats - Binary's features column names
-        :param adapted_nn: TensorFlow/Keras neural network.
-                            Neural network weights can be accessed by using: adapted_nn.get_weights(), for more info
-                            please, refer to TensorFlow/Keras documentation
-        :param df_factual: Dataset with the factual rows to be tested
-        :param factual: A simple list of the factual result to be tested
-        :param factual_oh: Same as factual but one-hot encoded, IF THERE'S NO CATEGORICAL FEATURES IT'S THE SAME
-        AS factual
-        :param session: TensorFlow current session
-
-        :return: 2 outputs:
-            (list) - A simple list with the counterfactual result. It can be one-hot encoded or not.
-                     If a counterfactual is not found, return an empty list.
-            (float) - Time used to generate a counterfactual
-        """
-        raise (NotImplementedError, 'Please, implement this method')
 
     def create_generator(
             self,
@@ -70,7 +32,74 @@ class BenchmarkCF:
             dataset_idx = [*range(22)]
 
         return BenchmarkGenerator(
-            framework_name=self.framework_name,
             output_number=self.output_number,
             ds_id_test=dataset_idx,
             disable_gpu=self.disable_gpu)
+
+
+def process_benchmark(algorithm_name: str):
+    algorithm_dfs = []
+    for f in os.listdir('./cfbench_results'):
+        if '_'.join(f.split('_')[:-3]) == algorithm_name:
+            algorithm_dfs.append(pd.read_pickle(f'./cfbench_results/{f}')[0])
+    if len(algorithm_dfs) != total_factual:
+        raise ValueError(f'Number of results ({len(algorithm_dfs)}) '
+                         f'is different from the number of factuals ({total_factual})')
+
+    algorithm_df = pd.DataFrame(algorithm_dfs)
+
+    if not os.path.exists(f'./cfbench_results_processed/'):
+        os.mkdir(f'./cfbench_results_processed/')
+
+    algorithm_df.to_pickle(f'./cfbench_results_processed/{algorithm_name}.pkl')
+
+
+def send_results(algorithm_name: str):
+    # Create result dataframe if not existent
+    if not os.path.exists(f'./cfbench_results_processed/{algorithm_name}.pkl'):
+        process_benchmark(algorithm_name)
+
+    # Ask the user to fork the repository
+    print('Please, fork the following repository: https://github.com/rmazzine/Ranking-Tabular-CF.git')
+    input('Press enter to continue:')
+
+    # Ask the user for the link of the forked repository
+    repository_link = input('Please, enter the link of the forked repository: ')
+
+    repository_folder = repository_link.split('/')[-1].split('.git')[0]
+
+    print('Cloning the repository...')
+    if os.path.exists(repository_folder):
+        shutil.rmtree(repository_folder)
+    os.system(f'git clone {repository_link}')
+
+    # Create a new branch with the algorithm_name
+    print('Creating a new branch with the algorithm_name...')
+    os.system(f'git checkout -b {algorithm_name}')
+
+    # Copy the results to the repository
+    print('Copying the results to the repository...')
+    if os.path.exists(f'{repository_folder}/results/{algorithm_name}'):
+        print('Removing old results...')
+        shutil.rmtree(f'{repository_folder}/results/{algorithm_name}')
+    else:
+        os.mkdir(f'{repository_folder}/results/{algorithm_name}')
+
+    # Copy the result dataframe to the repository
+    shutil.copyfile(f'./cfbench_results_processed/{algorithm_name}.pkl',
+                    f'{repository_folder}/results/{algorithm_name}/{algorithm_name}.pkl')
+
+    # Push the results to the repository
+    print('Pushing the results to the repository...')
+    os.system(f'cd {repository_folder}')
+    os.system(f'cd {repository_folder} git add .')
+    os.system(f'cd {repository_folder} git commit -m "Update results"')
+    os.system(f'cd {repository_folder} git push')
+
+    # Make a pull request to the repository
+    print('Now, go to your forked repository and make a pull request to the main branch')
+    print(f'Your forked repo link: {repository_link}/tree/{algorithm_name}')
+
+    # Remove the repository
+    shutil.rmtree(repository_folder)
+
